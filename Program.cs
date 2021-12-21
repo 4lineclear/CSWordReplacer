1e6614e8adb8bce69eb0b18697a2436863b4ee81
yahya <tamimhakimi21@gmail.com>
2021-12-21 01:39:27 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace CSWordReplacer
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Replacer replacer = new Replacer("onetwothreeonethreeo");
+            Console.WriteLine(replacer.wordReplace("one", "two"));
+            Console.WriteLine(Replacer.StaticWordReplace("onetwothreeonethreeo", "one", "two"));
+        }
+    }
+}
---
+++ Replacer.cs
@@ -0,0 +1,54 @@
+﻿namespace CSWordReplacer
+{
+    public class Replacer
+    {
+        private string word;
+        public Replacer()
+        {
+            this.word = "";
+        }
+        public Replacer(string word)
+        {
+            this.word = word;
+        }
+        public string wordReplace(string find, string replace)
+        {
+            word = StaticWordReplace(word, find, replace);
+            return word;
+        }
+        public static string StaticWordReplace(string word, string find, string replace)
+        {
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i] == find[0])
+                {
+                    if (checkString(word.Substring(i), find))
+                    {
+                        word = word.Remove(i, find.Length);
+                        word = word.Insert(i, replace);
+                    }
+                }
+            }
+            return word;
+        }
+        public static bool checkString(string word, string find)
+        {
+            if (word.Length < find.Length)
+            {
+                return false;
+            }
+            for (int i = 0; i < find.Length; i++)
+            {
+                if (word[i] != find[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public string getWord()
+        {
+            return word;
+        }
+    }
+}
